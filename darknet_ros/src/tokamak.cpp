371db202d2648aba1bb577bd5c5b3bbc445e94ba
Sasha Lew <a.lew@yale.edu>
2023-08-16 09:17:02 -0400

    adjust tokamak queue sizes

--- darknet_ros/src/tokamak.cpp
+++ darknet_ros/src/tokamak.cpp
@@ -3,18 +3,18 @@
 
 Tokamak::Tokamak(const ros::NodeHandle& nh) : nh_(nh),
   it_(nh_),
-  master_im_sub_(it_, "/kinect/master/rgb/image_raw", 3),
-  sub_im_sub_(it_, "/kinect/sub/rgb/image_raw", 3),
-  sync_(SerialImagePolicy(15), master_im_sub_, sub_im_sub_)
+  master_im_sub_(it_, "/kinect/master/rgb/image_raw", 15),
+  sub_im_sub_(it_, "/kinect/sub/rgb/image_raw", 15),
+  sync_(SerialImagePolicy(30), master_im_sub_, sub_im_sub_)
 {
-  image_fusion_pub_ = it_.advertise("/kinect/combined/image_raw", 6);
+  image_fusion_pub_ = it_.advertise("/kinect/combined/image_raw", 30);
   sync_.registerCallback(boost::bind(&Tokamak::filterCallback, this, _1, _2));
 }
 
 void Tokamak::filterCallback(const sensor_msgs::ImageConstPtr& master_im, const sensor_msgs::ImageConstPtr& sub_im)
 {
   image_fusion_pub_.publish(master_im);
-  ros::Duration(0.03).sleep();
+  ros::Duration(0.0333).sleep();
   image_fusion_pub_.publish(sub_im);
 }
 
